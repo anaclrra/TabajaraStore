be3da7453b5be2212261a50db2bf2974c235bf19
Ana Clara <anaclarab826@gmail.com>
2023-09-20 10:26:47 -0300

    implementando crud

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/VendasEstoqueApplication.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/VendasEstoqueApplication.java
@@ -10,4 +10,5 @@ public class VendasEstoqueApplication {
 		SpringApplication.run(VendasEstoqueApplication.class, args);
 	}
 
+	
 }
--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
@@ -1,11 +1,14 @@
 package br.edu.ifrn.vendasestoque.domain.categoria;
 
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,11 +21,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class Categoria {
+public class Categoria implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    //@Pattern(regexp = "")
     private String nome;
 
 }
